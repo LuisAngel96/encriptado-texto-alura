3828fe418fc322af7f17bdfa9891e60944722336
LuisAngel96 <lavalle1996@gmail.com>
2024-03-02 21:42:08 -0500

    Creacion de funcion Encriptar-app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+const textoNingun = document.querySelector(".mensaje-encontrado");
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+    textoNingun.style.visibility = "hidden";
+
+}
+
+function encriptar(stringEncriptada){
+
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]) ;
+        }
+    }
+    return stringEncriptada;
+}
+
