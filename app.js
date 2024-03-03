624d542c3244789e4ad6764efb8731be993801dd
LuisAngel96 <lavalle1996@gmail.com>
2024-03-02 21:43:25 -0500

    Creacion de funcion desencriptar

--- app.js
+++ app.js
@@ -32,3 +32,25 @@ function encriptar(stringEncriptada){
     return stringEncriptada;
 }
 
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+    textoNingun.style.visibility = "hidden";
+}
+
+function desencriptar(stringDesencriptado){
+
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptado = stringDesencriptado.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptado.includes(matrizCodigo[i][1])){
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+    return stringDesencriptado;
+
+    
+}
